8f9aa67813f5651587cd5fcab840ceabaae20fc8
Akash Pandey <akash@escapp.in>
2021-09-30 18:54:39 +0530

    Update app.e2e-spec.ts

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('bjs-resume-fe App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!')
   });
 });
